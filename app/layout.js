64d203f51fe988586861af514dd95d086522a52e
SBIA-Younes <brahim-sb@hotmail.fr>
2024-03-17 23:44:12 +0100

    initialisation progect

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
+import Header from "./_components/Header";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={outfit.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
